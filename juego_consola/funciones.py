bb3b019609b3acdf94bf8d371d4ab2895216c261
Nasho <NachoFonta12@gmail.com>
2024-11-11 12:24:37 -0300

    Division de archivos y nuevas funciones

---
+++ juego_consola/funciones.py
@@ -0,0 +1,74 @@
+import random
+def generar_lista_diccionario(path):
+    with open(path,"r") as archivo_cartas:
+        lista_diccionarios = []
+        for linea in archivo_cartas:
+            diccionario_carta = {}
+            linea = linea.split(",")
+            diccionario_carta["nombre"] = linea[0]
+            diccionario_carta["velocidad"] = linea[1]
+            diccionario_carta["fuerza"] = linea[2]
+            diccionario_carta["elemento"] = linea[3]
+            diccionario_carta["peso"] = linea[4]
+            diccionario_carta["altura"] = linea[5]
+            lista_diccionarios.append(diccionario_carta)
+    return lista_diccionarios
+
+def mezclar_barajas(baraja: list)->list:
+    baraja = random.shuffle(baraja)
+    return baraja
+
+def generar_elementos(elementos:dict)->dict:
+    for clave in elementos:
+        elementos[clave] = random.randint(100,200)
+    return elementos
+
+def asignar_nombres()->list:
+    lista_jugadores = []
+    for i in range(2):
+        jugador = str(input("Ingrese su nombre: "))
+        lista_jugadores.append(jugador)
+    return lista_jugadores
+
+def sortear_atributo(lista: list)->str:
+    diccionario = lista[0]
+    atributo = random.choice(list(diccionario.keys()))
+    while atributo == "nombre":
+        atributo = random.choice(list(diccionario.keys()))
+    return atributo
+
+def leer_lista(lista: list):
+    for i in range(len(lista)):
+        print(lista[i])
+
+def comparar_cartas(lista1: list, lista2: list, atributo: str):
+    ganador = False
+    carta_jugador_uno = lista1.pop(0)
+    carta_jugador_dos = lista2.pop(0)
+    print(carta_jugador_uno[atributo])
+    print(carta_jugador_dos[atributo])
+    if carta_jugador_uno[atributo] > carta_jugador_dos[atributo]:
+        lista1.append(carta_jugador_dos)
+        lista1.append(carta_jugador_uno)
+        ganador = True
+    elif carta_jugador_uno[atributo] < carta_jugador_dos[atributo]:
+        lista2.append(carta_jugador_dos)
+        lista2.append(carta_jugador_uno)
+        ganador = True
+    else:
+        while ganador == False:
+                carta_jugador_uno = lista1.pop(0)
+                carta_jugador_dos = lista2.pop(0)
+                if carta_jugador_uno[atributo] > carta_jugador_dos[atributo]:
+                    lista1.append(carta_jugador_dos)
+                    lista2.append(carta_jugador_uno)
+                    ganador = True
+                elif carta_jugador_uno[atributo] < carta_jugador_dos[atributo]:
+                    lista2.append(carta_jugador_dos)
+                    lista2.append(carta_jugador_uno)
+                    ganador = True
+    #leer_lista(lista1)
+    print(len(lista1))
+    print("-" * 100)
+    #leer_lista(lista2)
+    print(len(lista2))
--- juego_consola/main.py
+++ juego_consola/main.py
@@ -1,81 +1,11 @@
-import random
-def generar_elementos(elementos:dict)->dict:
-
-    for clave in elementos:
-        elementos[clave] = random.randint(100,200)
-
-    return elementos
-
-
-with open("MOCK_DATA.csv","r") as archivo_cartas:
-    diccionario_carta = {}
-    # {"nombre":"",
-    #  "velocidad":0,
-    #  "fuerza":0,
-    #  "elemento":"agua",
-    #  "peso":0,
-    #  "altura":0}
-    for linea in archivo_cartas:
-        linea = linea.split(",|\n")
-        diccionario_carta["nombre"] = linea[0]
-        diccionario_carta["velocidad"] = linea[1]
-        diccionario_carta["fuerza"] = linea[2]
-        diccionario_carta["elemento"] = linea[3]
-        diccionario_carta["nombre"] = linea[4]
-        diccionario_carta["nombre"] = linea[4]
-
+from funciones import *
+path = "C:/Users/Usuario/Desktop/Repositorio/Programacion_I_Juego/juego_consola/MOCK_DATA.csv"
 elementos = {"agua":0 ,"tierra":0, "aire":0, "fuego":0, "electricidad":0}
-# jugador_uno = ""
-# jugador_dos = ""
 
-# jugador_uno = input("Ingrese su nombre: ")
-# jugador_dos = input("Ingrese su nombre: ")
-
-listas_cartas = [
-    {"nombre":"",
-     "velocidad":0,
-     "fuerza":0,
-     "elemento":"agua",
-     "peso":0,
-     "altura":0},
-
-     {"nombre":"",
-     "velocidad":0,
-     "fuerza":0,
-     "elemento":"tierra",
-     "peso":0,
-     "altura":0},
-
-     {"nombre":"",
-     "velocidad":0,
-     "fuerza":0,
-     "elemento":"fuego",
-     "peso":0,
-     "altura":0},
-
-     {"nombre":"",
-     "velocidad":0,
-     "fuerza":0,
-     "elemento":"agua",
-     "peso":0,
-     "altura":0},
-
-     {"nombre":"",
-     "velocidad":0,
-     "fuerza":0,
-     "elemento":"aire",
-     "peso":0,
-     "altura":0},
-
-     {"nombre":"",
-     "velocidad":0,
-     "fuerza":0,
-     "elemento":"tierra",
-     "peso":0,
-     "altura":0}
-]
-
-
-
-
-
+lista_cartas = generar_lista_diccionario(path)
+lista_cartas_2 = generar_lista_diccionario(path)
+mezclar_barajas(lista_cartas_2)
+#lista_nombres = asignar_nombres()
+atributo = sortear_atributo(lista_cartas)
+print(atributo)
+comparar_cartas(lista_cartas, lista_cartas_2, atributo)
